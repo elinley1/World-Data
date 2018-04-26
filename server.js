9028a9dfccdd02f9f024d4e1314ea28741ea089f
Siyan Zhou <siyanzhou@lawn-128-61-21-80.lawn.gatech.edu>
2018-04-26 10:15:25 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,34 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+var db = require("./models");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
+app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
