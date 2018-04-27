9d012f0b75dad27798ce576fa1d71c17978b9a80
Siyan Zhou <siyanzhou@lawn-128-61-29-41.lawn.gatech.edu>
2018-04-27 10:42:16 -0400

    db

--- DB/happiness.sql
+++ DB/happiness.sql
@@ -2,33 +2,33 @@ CREATE TABLE happiness (
     country VARCHAR (100) NOT NULL,
     code VARCHAR (3) NOT NULL,
     year INT NOT NULL,
-    happiness_idx DECIMAL (5, 4) NOt NULL
+    happiness_idx DECIMAL (60, 0) NOt NULL
 );
 
 CREATE TABLE gdp (
     country VARCHAR (100) NOT NULL,
     code VARCHAR (3) NOT NULL,
     year INT NOT NULL,
-    real_gdp DECIMAL (5,4) NOT NULL
+    real_gdp DECIMAL (60,0) NOT NULL
 );
 
 CREATE TABLE gini (
     country VARCHAR (100) NOT NULL,
     code VARCHAR (3) NOT NULL,
     year INT NOT NULL,
-    gini_idx DECIMAL (5,4) NOT NULL
+    gini_idx DECIMAL (60,0) NOT NULL
 );
 
 CREATE TABLE life (
     country VARCHAR (100) NOT NULL,
     code VARCHAR (3) NOT NULL,
     year INT NOT NULL,
-    life_span DECIMAL (5,4) NOT NULL
+    life_span DECIMAL (60,0) NOT NULL
 );
 
 CREATE TABLE internet (
     country VARCHAR (100) NOT NULL,
     code VARCHAR (3) NOT NULL,
     year INT NOT NULL,
-    internet_use DECIMAL (5,4) NOT NULL
+    internet_use DECIMAL (60,0) NOT NULL
 );
