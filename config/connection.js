d1e06823f5b9537edb9c2a8c24f6098d487bf1ec
Erin Linley <elinley1@gmail.com>
2018-04-27 12:19:05 -0400

    sequelize db connected with models

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("world_data", "ooy00duznsn0hht8", "doq8rlras8j0tvqq", {
+    host: "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      },
+});
+
+module.exports = sequelize;
